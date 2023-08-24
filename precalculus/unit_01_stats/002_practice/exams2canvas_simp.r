5fd1ef6e4b116ba2cd2366b449330682eb5e1f8d
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2023-08-23 20:10:44 -0400

    Sorry:

--- precalculus/unit_01_stats/002_practice/exams2canvas_simp.r
+++ precalculus/unit_01_stats/002_practice/exams2canvas_simp.r
@@ -1,11 +1,10 @@
 library("exams")
 n = 30
 outdir = "outcanvas"
-exams2canvas("ET_02.rmd",
+exams2canvas(c("calc_z.rmd","calc_EV.rmd"),
              n=n,
              dir=outdir,
-             points=5,
-             name="ET_02",
+             points=2,
+             name="02_calculate_z_and_mu_of_discrete_variable",
              maxattempts = 1,
              template = "canvas_qti12.xml")
-
---
+++ precalculus/unit_01_stats/003_z_prob/exams2canvas_simp.r
@@ -0,0 +1,25 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+
+probs = c("01_leftarea_from_z.Rmd",
+          "02_rightarea_from_z.Rmd",
+          "03_centralarea_from_z.Rmd",
+          "04_twotailarea_from_z.Rmd",
+          "05_area_between_two_zs.Rmd",
+          "06_z_from_left_area.Rmd",
+          "07_z_from_right_area.Rmd",
+          "08_z_from_central_area.Rmd",
+          "09_z_from_twotail_area.Rmd")
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0("003_",stems)
+
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=2,
+               maxattempts = Inf,
+               name=names[i],
+               template = "canvas_qti12.xml")
+}
