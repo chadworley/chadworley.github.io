cd9ebe67844b21774fd9318f3df2eeca091ec6de
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2023-08-28 20:05:56 -0400

    Sorry: I am working on it

--- precalculus/unit_01_stats/week_02/005_sd/exams2canvas_simp.r
+++ precalculus/unit_01_stats/week_02/005_sd/exams2canvas_simp.r
@@ -8,22 +8,30 @@ probs = c("01_sd_pop_int.Rmd",
           "04_estimate_SD.Rmd",
           "05_pop_sum_two_lotteries.Rmd")
 
-stems = tools::file_path_sans_ext(sub(".*/", "", probs))
-names = paste0("005_",stems)
-for(i in 1:length(probs)){
-  exams2canvas(probs[i],
-               n=n,
-               dir=outdir,
-               points=2,
-               maxattempts = 20,
-               name=names[i],
-               template = "canvas_qti12.xml")
-}
+# stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+# names = paste0("005_",stems)
+# for(i in 1:length(probs)){
+#   exams2canvas(probs[i],
+#                n=n,
+#                dir=outdir,
+#                points=2,
+#                maxattempts = 20,
+#                name=names[i],
+#                template = "canvas_qti12.xml")
+# }
 
-exams2canvas(probs,
-               n=n,
-               dir=outdir,
-               points=2,
-               maxattempts = 5,
-               name="005_st_dev_mixed",
-               template = "canvas_qti12.xml")
+# exams2canvas(probs,
+#                n=n,
+#                dir=outdir,
+#                points=2,
+#                maxattempts = 5,
+#                name="005_st_dev_mixed",
+#                template = "canvas_qti12.xml")
+
+exams2html(probs,
+           n=1,
+           dir=outdir,
+           name="005_shared_questions",
+           converter = "pandoc-mathjax",
+           mathjax = TRUE,
+           template = "plain.html")
