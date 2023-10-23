07d2a6e8c3f9d177b78920764757e7465518568a
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2023-10-23 08:12:44 -0400

    Sorry: rational

---
+++ precalculus/016_polynomial_and_rational/exams2canvas_mixed.r
@@ -0,0 +1,35 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+
+genname = "016"
+
+probs = c("01_synthetic_division.Rmd")
+
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0(genname,"_",stems)
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=2,
+               maxattempts = 20,
+               name=names[i],
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+}
+
+
+# probs = c("match_factored_form_to_graph.Rmd",
+#           "match_factored_form_to_graph.Rmd",
+#           "match_factored_form_to_graph.Rmd")
+# 
+# exams2canvas(probs,
+#                n=n,
+#                dir=outdir,
+#                points=3,
+#                maxattempts = 6,
+#                name=paste0(genname,"_mastery"),
+#                template = "canvas_qti12.xml",
+#                cloze = list(enumerate = FALSE))
+
