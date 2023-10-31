a65f4d92f22ce2636b589541482118d9eff7973b
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2023-10-31 08:39:28 -0400

    Sorry: rational

--- precalculus/016_polynomial_and_rational/exams2canvas_mixed.r
+++ precalculus/016_polynomial_and_rational/exams2canvas_mixed.r
@@ -4,7 +4,22 @@ outdir = "outcanvas"
 
 genname = "016"
 
-probs = c("16_solve_polynomial_complex_roots.Rmd")
+probs = c("01_synthetic_division.Rmd",
+          "02_VA_zero_hole_HA.Rmd",
+          "03_quadratic_formula.Rmd",
+          "04_quadratic_formula_complex.Rmd",
+          "05_solve_cubic_easy_integers.Rmd",
+          "06_solve_cubic_one_real.Rmd",
+          "07_polynomial_ends.Rmd",
+          "08_add_complex.Rmd",
+          "09_subtract_complex.Rmd",
+          "10_multiply_complex.Rmd",
+          "11_read_complex_plot.Rmd",
+          "12_complex_modulus.Rmd",
+          "13_complex_angle.Rmd",
+          "14_complex_polar_to_rect.Rmd",
+          "15_multiply_complex_polar.Rmd",
+          "16_solve_polynomial_complex_roots.Rmd")
 
 stems = tools::file_path_sans_ext(sub(".*/", "", probs))
 names = paste0(genname,"_",stems)
@@ -19,11 +34,13 @@ for(i in 1:length(probs)){
                cloze = list(enumerate = FALSE))
 }
 
-
+# genname = "complex_numbers"
+# 
 # probs = c("08_add_complex.Rmd",
 #           "09_subtract_complex.Rmd",
 #           "10_multiply_complex.Rmd",
-#           "11_read_complex_plot.Rmd")
+#           "11_read_complex_plot.Rmd",
+#           "16_solve_polynomial_complex_roots.Rmd")
 # 
 # exams2canvas(probs,
 #                n=n,
@@ -33,4 +50,4 @@ for(i in 1:length(probs)){
 #                name=paste0(genname,"_mastery"),
 #                template = "canvas_qti12.xml",
 #                cloze = list(enumerate = FALSE))
-
+# 
