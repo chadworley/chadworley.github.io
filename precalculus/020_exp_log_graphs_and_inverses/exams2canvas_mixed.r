4a4d58e9123d16ffce97e6548d70f2934a73844c
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2023-11-19 23:31:48 -0500

    Sorry: euler_method

---
+++ precalculus/020_exp_log_graphs_and_inverses/exams2canvas_mixed.r
@@ -0,0 +1,35 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+
+genname = "020"
+
+probs = c("01_solve_exponent.Rmd",
+          "02_cooling_solve_time.Rmd",
+          "03_eulers_method_water.Rmd")
+
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0(genname,"_",stems)
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=2,
+               maxattempts = 20,
+               name=names[i],
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+}
+
+# probs = list(probs)
+# probs = rep(probs,10)
+# 
+# exams2canvas(probs,
+#                n=n,
+#                dir=outdir,
+#                points=3,
+#                maxattempts = 6,
+#                name=paste0(genname,"_mastery"),
+#                template = "canvas_qti12.xml",
+#                cloze = list(enumerate = FALSE))
+
