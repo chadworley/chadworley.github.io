cf31c895379161679bfcc8ad1cf9c08fe8d71312
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2023-12-18 09:17:43 -0500

    Sorry: trig

--- precalculus/022_radians_degrees/exams2canvas_mixed.r
+++ precalculus/022_radians_degrees/exams2canvas_mixed.r
@@ -4,18 +4,7 @@ outdir = "outcanvas"
 
 genname = "022"
 
-probs = c("01_standard_pos_deg.Rmd",
-          "02_large_angle_deg.Rmd",
-          "03_coterminal_degs.Rmd",
-          "04_convert_degs_to_rots.Rmd",
-          "05_convert_rots_to_degs.Rmd",
-          "06_convert_degs_to_rads.Rmd",
-          "07_convert_rads_to_degs.Rmd",
-          "08_arc_length_degs.Rmd",
-          "09_arc_length_rads.Rmd",
-          "10_plot_angle_rads.Rmd",
-          "11_plot_coterminal_rads.Rmd",
-          "12_large_angle_rad.Rmd")
+probs = c("07_convert_rads_to_degs.Rmd")
 
 stems = tools::file_path_sans_ext(sub(".*/", "", probs))
 names = paste0(genname,"_",stems)
