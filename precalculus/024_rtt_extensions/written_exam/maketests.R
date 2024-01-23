a7a4eaa06c305c9f3cd103ca344fe865d6353c71
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2024-01-22 21:03:54 -0500

    Sorry: rtt_extensions

--- precalculus/024_rtt_extensions/written_exam/maketests.R
+++ precalculus/024_rtt_extensions/written_exam/maketests.R
@@ -1,10 +1,10 @@
-set.seed(111)
+set.seed(245)
 
-for(i in 1:5){
+for(i in 1:30){
   myseed = round(runif(1,1111,9999))
   set.seed(myseed)
-  rmarkdown::render('024_written_exam.Rmd', output_file = paste0("output/024_WE",i,'.pdf'),params = list(num = myseed))
+  rmarkdown::render('024_written_exam.Rmd', output_file = paste0("output/024_WE",myseed,'.pdf'),params = list(num = myseed))
   set.seed(myseed)
-  rmarkdown::render('024_written_exam_sol.Rmd', output_file = paste0("output/024_WE",i,'_sol.pdf'),params = list(num = myseed))
+  rmarkdown::render('024_written_exam_sol.Rmd', output_file = paste0("output/024_WE",myseed,'_sol.pdf'),params = list(num = myseed))
 }
 
