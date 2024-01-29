9872d0f3db71d447322ad51e66b3e7536c3520bd
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-01-29 10:23:34 -0500

    Sorry: trig_funcs

---
+++ alg_1_topics/data_sd/exams2canvas_mixed.r
@@ -0,0 +1,30 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+
+genname = "w23"
+probs = c("01_sd_pop_int.Rmd",
+          "02_estimate_SD.Rmd")
+
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0(genname,"_",stems)
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=2,
+               maxattempts = 20,
+               name=names[i],
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+}
+
+exams2canvas(probs,
+               n=n,
+               dir=outdir,
+               points=3,
+               maxattempts = 6,
+               name=paste0(genname,"_mastery"),
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+
