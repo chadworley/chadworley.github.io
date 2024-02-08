87c2f5c332343d64292cc84222b867a9d9d66c54
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2024-02-08 06:54:33 -0500

    Sorry: trig_idents

--- precalculus/026_trig_identities/exams2canvas_mixed.r
+++ precalculus/026_trig_identities/exams2canvas_mixed.r
@@ -4,7 +4,7 @@ outdir = "outcanvas"
 
 genname = "026"
 
-probs = c("05_LOC_get_angle.Rmd")
+probs = c("06_angle_sum_ident.Rmd")
 
 stems = tools::file_path_sans_ext(sub(".*/", "", probs))
 names = paste0(genname,"_",stems)
--- precalculus/026_trig_identities/exams2html_interactive.r
+++ precalculus/026_trig_identities/exams2html_interactive.r
@@ -6,7 +6,8 @@ probs = c("01_area_of_triangle.Rmd",
           "02_law_of_sines.Rmd",
           "03_LOS_ambig_case.Rmd",
           "04_LOC_get_side.Rmd",
-          "05_LOC_get_angle.Rmd")
+          "05_LOC_get_angle.Rmd",
+          "06_angle_sum_ident.Rmd")
 
 nvers = 10
 ps = character(0)
