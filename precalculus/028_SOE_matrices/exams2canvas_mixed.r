cf6bdaf515ad6566aa9e9b321f340e2eb1f5e1ae
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-04-01 08:35:01 -0400

    Sorry: conics

--- precalculus/028_SOE_matrices/exams2canvas_mixed.r
+++ precalculus/028_SOE_matrices/exams2canvas_mixed.r
@@ -3,28 +3,35 @@ n = 30
 outdir = "outcanvas"
 genname = "028"
 
-probs = c("17_row_ops_equiv_mat_mult.Rmd")
-
-stems = tools::file_path_sans_ext(sub(".*/", "", probs))
-names = paste0(genname,"_",stems)
-for(i in 1:length(probs)){
-  exams2canvas(probs[i],
-               n=n,
-               dir=outdir,
-               points=4,
-               maxattempts = 15,
-               name=names[i],
-               template = "canvas_qti12.xml",
-               cloze = list(enumerate = FALSE))
-}
-
-# probs = c("")
+# probs = c("17_row_ops_equiv_mat_mult.Rmd")
 # 
-# exams2canvas(probs,
-#              n=n,
-#              dir=outdir,
-#              points=3,
-#              maxattempts = 6,
-#              name=paste0(genname,"_mastery"),
-#              template = "canvas_qti12.xml",
-#              cloze = list(enumerate = FALSE))
+# stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+# names = paste0(genname,"_",stems)
+# for(i in 1:length(probs)){
+#   exams2canvas(probs[i],
+#                n=n,
+#                dir=outdir,
+#                points=4,
+#                maxattempts = 15,
+#                name=names[i],
+#                template = "canvas_qti12.xml",
+#                cloze = list(enumerate = FALSE))
+# }
+
+probs = list(c("01_SOE_matrix_form.Rmd","02_SOE_augmented_form.Rmd"),
+             "06_RREF_sagemath.Rmd",
+             "08_check_if_inverse.Rmd",
+             "11_use_inverse_to_solve.Rmd",
+             "13_determinant_3x3.Rmd",
+             "15_can_solve_sys.Rmd",
+             "17_row_ops_equiv_mat_mult.Rmd"
+             )
+
+exams2canvas(probs,
+             n=n,
+             dir=outdir,
+             points=c(3,3,4,4,4,3,4),
+             maxattempts = 6,
+             name=paste0(genname,"_mastery"),
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
