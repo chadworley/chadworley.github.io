5a6eb5d56a4ceab4d5d3fb3f07b9d045c9d2e695
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-04-07 21:30:39 -0400

    Sorry: conics

--- precalculus/029_conic_sections/written_exam/make_tests.R
+++ precalculus/029_conic_sections/written_exam/make_tests.R
@@ -1,25 +1,25 @@
-dr = "out_practice_sol"
-#set.seed(112)
+# dr = "out_practice_sol"
+# dir.create(dr, showWarnings=FALSE)
+# for(i in 1:30){
+#   myseed = i #round(rsunif(1,1111,9999))
+#   set.seed(myseed)
+#   rmarkdown::render('029_written_exam.Rmd', 
+#                     output_file = paste0(dr,"/029_WE_pracSol_",sprintf("%04d",myseed),'.pdf'),
+#                     params = list(num = myseed,
+#                                   mytitle = "PC 029 Practice Exam",
+#                                   showsol = "True"))
+# }
+
+dr = "out_practice"
 dir.create(dr, showWarnings=FALSE)
 for(i in 1:30){
-  myseed = i #round(rsunif(1,1111,9999))
+  myseed = i
   set.seed(myseed)
   rmarkdown::render('029_written_exam.Rmd', 
-                    output_file = paste0(dr,"/029_WE_pracSol_",sprintf("%04d",myseed),'.pdf'),
+                    output_file = paste0(dr,"/029_WE_prac_",sprintf("%04d",myseed),'.pdf'),
                     params = list(num = myseed,
                                   mytitle = "PC 029 Practice Exam",
-                                  showsol = "True"))
+                                  showsol = "False"))
 }
-
-# 
-# dir.create("out_prac", showWarnings=FALSE)
-# for(i in 1:10){
-#   myseed = i
-#   set.seed(myseed)
-#   rmarkdown::render('026_written_exam.Rmd',
-#                     output_file = paste0("out_prac/026_WE",myseed,'.pdf'),
-#                     params = list(num = myseed,
-#                                   mytitle = "PC 026 NO-CALC Practice"))
-# }
 
 
