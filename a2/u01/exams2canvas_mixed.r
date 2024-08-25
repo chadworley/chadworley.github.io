e023a33ff4ddc7fe99fb9b9f245562c6c2820efc
Chad Worley <chad.worley@bartcharter.org@ChadWorhool2023.lan>
2024-08-25 14:36:57 -0400

    hmm hi

--- a2/u01/exams2canvas_mixed.r
+++ a2/u01/exams2canvas_mixed.r
@@ -3,19 +3,9 @@ n = 30
 outdir = "outcanvas"
 genname = "u01_"
 
-probs = c("01_sam_stat_pop_prop.Rmd",
-          "02_survey_experi_obsstu.Rmd",
-          "03_sample_bias.Rmd",
-          "04_corr_or_cause.Rmd",
-          "05_basic_data_analysis.Rmd",
-          "06_big_data_analysis.Rmd",
-          "07_read_histogram.Rmd",
-          "08_make_hist_see_shape.Rmd",
-          "09_ME_dice_tots.Rmd",
-          "10_prop_test_sim.Rmd",
-          "11_resample_mean_ME.Rmd")
+probs = c("08_make_hist_see_shape.Rmd")
 
-pnts = c(2,2,2,2,4,4,3,3,4,4,4)
+pnts = c(3)
 
 stems = tools::file_path_sans_ext(sub(".*/", "", probs))
 names = paste0(genname,stems)
