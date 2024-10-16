eb6c26305e09b8d4269f777219a858ec1f41ced8
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-10-16 09:52:35 -0400

    hmm hi

---
+++ ml/arithmetic_2/exams2canvas_mixed.r
@@ -0,0 +1,32 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+genname = "arith_"
+probs = c("long_division.Rmd")
+
+pnts = c(8)
+
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0(genname,stems)
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=pnts[i],
+               maxattempts = 10,
+               name=names[i],
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+}
+
+# probs = list("multiply_2digits.Rmd",
+#              "multiply_2digits.Rmd")
+# 
+# exams2canvas(probs,
+#              n=n,
+#              dir=outdir,
+#              points=c(5,5),
+#              maxattempts = 6,
+#              name=paste0(genname,"_mastery"),
+#              template = "canvas_qti12.xml",
+#              cloze = list(enumerate = FALSE))
