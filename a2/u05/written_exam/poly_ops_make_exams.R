0108ab89d508ed2cbe18134200c726dedd261b68
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-10-21 08:02:02 -0400

    hmm hi

--- a2/u05/written_exam/poly_ops_make_exams.R
+++ a2/u05/written_exam/poly_ops_make_exams.R
@@ -1,10 +1,10 @@
-for(jjj in 141:160){
+for(jjj in 200:250){
     rmarkdown::render(input = "poly_ops_WE.Rmd",
-                      output_file = paste0("prac_01/poe_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
+                      output_file = paste0("reals/poe_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
                                                                                     showsol = F,
                                                                                     titex = "EXAM"))
     rmarkdown::render(input = "poly_ops_WE.Rmd",
-                      output_file = paste0("prac_01/poe_sol_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
+                      output_file = paste0("reals/poe_sol_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
                                                                                     showsol = T,
                                                                                     titex = "SOLUTION"))
 }
