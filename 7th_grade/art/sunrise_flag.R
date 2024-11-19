5171c6d1e60a020ec8d4fb342b7966763bdfb851
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-11-19 08:29:43 -0500

    hmm hi

--- 7th_grade/art/sunrise_flag.R
+++ 7th_grade/art/sunrise_flag.R
@@ -5,9 +5,9 @@ par(mar=c(0,0,0,0))
 plot(0,0,"n",xlim=c(0,1),ylim=c(0,1),axes=F,ann=F)
 
 cols = c(rgb(0,0,0),
-         hsv(0.06,0.85,1),
-         rgb(0.5,0.4,0.5),
-         rgb(0.9,0.95,1))
+         hsv(0.06,0.75,1),
+         hsv(0.9,0.2,0.7),
+         hsv(0.55,0.2,1))
 
 for(i in 1:4){
     polygon(c(0,0,1,1),c((i-1)*0.25,i*0.25,i*0.25,(i-1)*0.25),col=cols[i],border=F)
---
+++ 7th_grade/art/sunset_east_flag.R
@@ -0,0 +1,16 @@
+pdf("sunset_east_colors.pdf",8,4)
+
+par(mar=c(0,0,0,0))
+
+plot(0,0,"n",xlim=c(0,1),ylim=c(0,1),axes=F,ann=F)
+
+cols = c(hsv(0.05,0.7,0.4),
+         hsv(0.55,0.2,1),
+         hsv(0.9,0.2,0.7),
+         hsv(0.06,0.75,1))
+
+for(i in 1:4){
+    polygon(c(0,0,1,1),c((i-1)*0.25,i*0.25,i*0.25,(i-1)*0.25),col=cols[i],border=F)
+}
+
+dev.off()
