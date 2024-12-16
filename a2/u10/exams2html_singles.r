85a3ce7dcdbb83a70eeba3bba029d657b98b7079
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-12-16 07:33:21 -0500

    hmm hi

--- a2/u10/exams2html_singles.r
+++ a2/u10/exams2html_singles.r
@@ -1,18 +1,20 @@
 library("exams")
 
-pppsss = c("01_sqrt_lake_area.Rmd",
-           "02_points_on_sqrtx.Rmd",
-           "03_translate_flip_sqrt.Rmd",
-           "04_sqrt_shift_points.Rmd",
-           "05_points_on_cbrtx.Rmd",
-           "06_translate_flip_cbrt.Rmd",
-           "07_sqrt_line_extraneous.Rmd",
-           "08_factrd_rational_featrs.Rmd",
-           "09_expndd_rational_featrs.Rmd",
-           "10_find_rational_hole.Rmd",
-           "11_line_thru_rational_hole.Rmd",
-           "12_distance_formula.Rmd",
-           "13_trilateration.Rmd")
+# pppsss = c("01_sqrt_lake_area.Rmd",
+#            "02_points_on_sqrtx.Rmd",
+#            "03_translate_flip_sqrt.Rmd",
+#            "04_sqrt_shift_points.Rmd",
+#            "05_points_on_cbrtx.Rmd",
+#            "06_translate_flip_cbrt.Rmd",
+#            "07_sqrt_line_extraneous.Rmd",
+#            "08_factrd_rational_featrs.Rmd",
+#            "09_expndd_rational_featrs.Rmd",
+#            "10_find_rational_hole.Rmd",
+#            "11_line_thru_rational_hole.Rmd",
+#            "12_distance_formula.Rmd",
+#            "13_trilateration.Rmd")
+
+pppsss = c("01_sqrt_lake_area.Rmd")
 
 for(ppss in pppsss){
     outdir = "outhtml"
