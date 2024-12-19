2623b33a94330a38ad971ea1b1007d9b491c4be0
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2024-12-19 07:49:32 -0500

    hmm hi

--- a2/u10/exams2canvas_mixed.r
+++ a2/u10/exams2canvas_mixed.r
@@ -3,37 +3,39 @@ n = 30
 outdir = "outcanvas"
 genname = "u10_"
 
-probs = c("17_radical_dom_rng.Rmd")
-pnts = c(3)
-
-stems = tools::file_path_sans_ext(sub(".*/", "", probs))
-names = paste0(genname,stems)
-for(i in 1:length(probs)){
-  exams2canvas(probs[i],
-               n=n,
-               dir=outdir,
-               points=pnts[i],
-               maxattempts = 10,
-               name=names[i],
-               template = "canvas_qti12.xml",
-               cloze = list(enumerate = FALSE))
-}
-
-
-# pm = list("01_sqrt_lake_area.Rmd",
-#           "17_radical_dom_rng.Rmd",
-#           "07_sqrt_line_extraneous.Rmd",
-#           "06_translate_flip_cbrt.Rmd",
-#           "09_expndd_rational_featrs.Rmd",
-#           "11_line_thru_rational_hole.Rmd",
-#           ""
-#           )
+# probs = c("17_radical_dom_rng.Rmd")
+# pnts = c(3)
 # 
-# exams2canvas(pm,
-#              n=n,
-#              dir=outdir,
-#              points=c(2,1,1,1,1,1,2,1),
-#              maxattempts = 6,
-#              name=paste0(genname,"_mastery"),
-#              template = "canvas_qti12.xml",
-#              cloze = list(enumerate = FALSE))
+# stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+# names = paste0(genname,stems)
+# for(i in 1:length(probs)){
+#   exams2canvas(probs[i],
+#                n=n,
+#                dir=outdir,
+#                points=pnts[i],
+#                maxattempts = 10,
+#                name=names[i],
+#                template = "canvas_qti12.xml",
+#                cloze = list(enumerate = FALSE))
+# }
+
+
+pm = list("m01_sqrt_lake_area.Rmd",
+          "m03_translate_flip_sqrt.Rmd",
+          "m17_radical_dom_rng.Rmd",
+          "m07_sqrt_line_extraneous.Rmd",
+          "m06_translate_flip_cbrt.Rmd",
+          "m09_expndd_rational_featrs.Rmd",
+          "m12_add_rationals.Rmd",
+          "m14_mult_rationals.Rmd",
+          "m16_inverse_rational.Rmd"
+          )
+
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=c(2,2,2,2,2,2,2,2,2),
+             maxattempts = 4,
+             name=paste0(genname,"_mastery"),
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
