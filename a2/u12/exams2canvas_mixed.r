02db2837440b5db61f18a9660780d525e62f1e71
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-01-10 08:01:07 -0500

    hmm hi

---
+++ a2/u12/exams2canvas_mixed.r
@@ -0,0 +1,49 @@
+library("exams")
+n = 30
+outdir = "outcanvas"
+genname = "u11_"
+
+# probs = c("23_round_trip_ave_speed.Rmd",
+#           "24_lagrange_polynomial.Rmd",
+#           "25_cubic_regression.Rmd",
+#           "26_predict_with_trendline.Rmd")
+# 
+# pnts = c(3,4,4,4)
+# 
+# stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+# names = paste0(genname,stems)
+# for(i in 1:length(probs)){
+#   exams2canvas(probs[i],
+#                n=n,
+#                dir=outdir,
+#                points=pnts[i],
+#                maxattempts = 10,
+#                name=names[i],
+#                template = "canvas_qti12.xml",
+#                cloze = list(enumerate = FALSE))
+# }
+
+
+pm = list("07_three_dim_dist.Rmd",
+          "12_dist_point_to_line.Rmd",
+          "18_circ_params_from_eq.Rmd",
+          "21_ellipse_get_pams.Rmd",
+          "03_points_in_overlap.Rmd",
+          "10_check_sols_ineq.Rmd",
+          "11_linear_programming.Rmd",
+          "13_tickets_soe.Rmd",
+          "14_resize_image_even_border.Rmd",
+          "m15_optimize_run_swim.Rmd",
+          "16_nth_score.Rmd",
+          "23_round_trip_ave_speed.Rmd",
+          "m25_cubic_regression.Rmd"
+          )
+
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=rep(2,length(pm)),
+             maxattempts = 4,
+             name=paste0(genname,"_mastery"),
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
