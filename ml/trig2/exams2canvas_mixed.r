14b839c564307310cab33aeafc70d9b9b38da4c5
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-02-25 09:59:17 -0500

    hmm hi

---
+++ ml/trig2/exams2canvas_mixed.r
@@ -0,0 +1,51 @@
+library("exams")
+n = 100
+outdir = "outcanvas"
+genname = "trig_"
+probs = c("01_shadow_speed.Rmd",
+          "02_sunset_shadow_speed.Rmd",
+          "03_bearing_distance_decomp.Rmd")
+
+pnts = rep(2,length(probs))
+
+stems = tools::file_path_sans_ext(sub(".*/", "", probs))
+names = paste0(genname,stems)
+for(i in 1:length(probs)){
+  exams2canvas(probs[i],
+               n=n,
+               dir=outdir,
+               points=pnts[i],
+               maxattempts = 10,
+               name=names[i],
+               template = "canvas_qti12.xml",
+               cloze = list(enumerate = FALSE))
+}
+
+# allqs = c("plus_times.Rmd",
+#           "plus_divide.Rmd",
+#           "minus_times.Rmd",
+#           "minus_divide.Rmd",
+#           "times_plus.Rmd",
+#           "times_minus.Rmd",
+#           "divide_plus.Rmd",
+#           "divide_minus.Rmd")
+
+# probs = list(allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs,
+#              allqs)
+# 
+# exams2canvas(probs,
+#              n=n,
+#              dir=outdir,
+#              points=c(2,2,2,2,2,2,2,2,2,2),
+#              maxattempts = 10,
+#              name=paste0(genname,"_mastery"),
+#              template = "canvas_qti12.xml",
+#              cloze = list(enumerate = FALSE))
