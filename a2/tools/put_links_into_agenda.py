f4f0d8e6b13775b0cbdc19472bebf52f73f8828e
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-02-25 21:20:45 -0500

    hmm hi

--- a2/tools/put_links_into_agenda.py
+++ a2/tools/put_links_into_agenda.py
@@ -5,11 +5,12 @@ import os
 
 
 front = "u14_"
-files = ["01_shadow_speed.Rmd",
-"02_sunset_shadow_speed.Rmd",
-"03_bearing_distance_decomp.Rmd",
-"04_rect_to_polar.Rmd",
-"05_add_vectors.Rmd"]
+files = ["06_turn_atan2_degs.Rmd",
+"07_turn_atan2_rads.Rmd",
+"08_short_turn_degs.Rmd",
+"09_short_turn_rads.Rmd",
+"10_rotate_point_degs.Rmd",
+"11_rotate_point_rads.Rmd"]
 
 practice_url_front = "https://chadworley.github.io/a2/u14/outhtml/"
 agenda = "/Users/chad.worley@bartcharter.org/Documents/chadworley.github.io/a2/weekly_a2.Rmd"
