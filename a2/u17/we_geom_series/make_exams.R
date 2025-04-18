72390819b85eb6593bffbecf1506f45ff3dc0b60
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-04-18 07:58:20 -0400

    hmm hi

--- a2/u17/we_geom_series/make_exams.R
+++ a2/u17/we_geom_series/make_exams.R
@@ -1,14 +1,14 @@
-for(jjj in 100:109){
+for(jjj in 300:400){
     rmarkdown::render(input = "geom_series_we.Rmd",
-                      output_file = paste0("examples/s17_geo_ser_prac_v",sprintf("%03d",jjj)),
+                      output_file = paste0("test/s17_geo_ser_prac_v",sprintf("%03d",jjj)),
                       params = list(rseed = jjj,
                                     showsol = F,
-                                    titex = "Example"))
+                                    titex = "EXAM"))
     rmarkdown::render(input = "geom_series_we.Rmd",
-                      output_file = paste0("examples/s17_geo_ser_prac_sol_v",sprintf("%03d",jjj)),
+                      output_file = paste0("test/s17_geo_ser_prac_sol_v",sprintf("%03d",jjj)),
                       params = list(rseed = jjj,
                                     showsol = T,
-                                    titex = "Solution"))
+                                    titex = "SLTN"))
 }
 
 # for(jjj in 600:699){
