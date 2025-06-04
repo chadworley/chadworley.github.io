edb585d8c2a100a8d6c29a97922378527ca2b9f3
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-06-04 15:07:22 -0400

    hmm hi

--- a2/u19/paper/make_exams.R
+++ a2/u19/paper/make_exams.R
@@ -1,44 +1,44 @@
 
-for(jjj in 1:10){
-    rmarkdown::render(input = "s19_written_exam_bw.Rmd",
-                      output_file = paste0("example_bw/s19_WEbw_example_v",sprintf("%03d",jjj)),
-                      params = list(rseed = jjj,
-                                    useseed = T,
-                                    showsol = F,
-                                    titex = "example"))
+# for(jjj in 1:10){
+#     rmarkdown::render(input = "s19_written_exam_bw.Rmd",
+#                       output_file = paste0("example_bw/s19_WEbw_example_v",sprintf("%03d",jjj)),
+#                       params = list(rseed = jjj,
+#                                     useseed = T,
+#                                     showsol = F,
+#                                     titex = "example"))
     # rmarkdown::render(input = "s19_written_exam_bw.Rmd",
     #                   output_file = paste0("example_bw/s19_WEbw_example_sol_v",sprintf("%03d",jjj)),
     #                   params = list(rseed = jjj,
     #                                 useseed = T,
     #                                 showsol = T,
     #                                 titex = "solution"))
-}
-
-for(jjj in 101:150){
-    rmarkdown::render(input = "s19_written_exam_bw.Rmd",
-                      output_file = paste0("practice_bw/s19_WEbw_practice_v",sprintf("%03d",jjj)),
-                      params = list(rseed = jjj,
-                                    useseed = T,
-                                    showsol = F,
-                                    titex = "practice"))
-    # rmarkdown::render(input = "s19_written_exam_bw.Rmd",
-    #                   output_file = paste0("practice_bw/s19_WEbw_practice_sol_v",sprintf("%03d",jjj)),
-    #                   params = list(rseed = jjj,
-    #                                 useseed = T,
-    #                                 showsol = T,
-    #                                 titex = "solution"))
-}
-
-# for(jjj in 600:699){
-#     rmarkdown::render(input = "trig_final.Rmd",
-#                       output_file = paste0("real/trig_final_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
-#                                                                                                    useseed = T,
-#                                                                                                   showsol = F,
-#                                                                                                   titex = "TEST"))
-#     rmarkdown::render(input = "trig_final.Rmd",
-#                       output_file = paste0("real/trig_final_sol_v",sprintf("%03d",jjj)), params = list(rseed = jjj,
-#                                                                                                        useseed = T,
-#                                                                                                       showsol = T,
-#                                                                                                       titex = "SLTN"))
 # }
+
+# for(jjj in 101:150){
+#     rmarkdown::render(input = "s19_written_exam_bw.Rmd",
+#                       output_file = paste0("practice_bw/s19_WEbw_practice_v",sprintf("%03d",jjj)),
+#                       params = list(rseed = jjj,
+#                                     useseed = T,
+#                                     showsol = F,
+#                                     titex = "practice"))
+#     rmarkdown::render(input = "s19_written_exam_bw.Rmd",
+#                       output_file = paste0("practice_bw/s19_WEbw_practice_sol_v",sprintf("%03d",jjj)),
+#                       params = list(rseed = jjj,
+#                                     useseed = T,
+#                                     showsol = T,
+#                                     titex = "solution"))
+# }
+
+for(jjj in 500:599){
+    rmarkdown::render(input = "s19_written_exam.Rmd",
+                      output_file = paste0("mastery/s19_WE_",sprintf("%03d",jjj)), params = list(rseed = jjj,
+                                                                                                   useseed = T,
+                                                                                                  showsol = F,
+                                                                                                  titex = "TEST"))
+    rmarkdown::render(input = "s19_written_exam.Rmd",
+                      output_file = paste0("mastery/s19_WE_sol_",sprintf("%03d",jjj)), params = list(rseed = jjj,
+                                                                                                       useseed = T,
+                                                                                                      showsol = T,
+                                                                                                      titex = "SLTN"))
+}
 
