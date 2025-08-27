7c406c4e941371743f80e74c4d2c5aee35363304
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-08-26 23:13:43 -0400

    hmm hi

--- algtwo2026/API_tools/upload_QTI_zip.py
+++ algtwo2026/API_tools/upload_QTI_zip.py
@@ -6,7 +6,7 @@ with open("//Users/chad.worley@bartcharter.org/Documents/canvasAPItests/acctok.t
 
 # Variables
 course_id = '2110'  
-file_path_list = ["outcanvas/CW_08_27_eval_funcs_and_invs.zip"]
+file_path_list = ["outcanvas/CW_08_27_create_inverse_func.zip"]
 
 ######## FOR UPLOADING ETS
 # fpl = []
