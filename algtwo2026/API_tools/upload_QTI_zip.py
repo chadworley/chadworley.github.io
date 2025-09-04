e24b680b0669cde4b3bdfb04cb5ce46022df6510
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-09-03 23:10:30 -0400

    hmm hi

--- algtwo2026/API_tools/upload_QTI_zip.py
+++ algtwo2026/API_tools/upload_QTI_zip.py
@@ -6,7 +6,7 @@ with open("//Users/chad.worley@bartcharter.org/Documents/canvasAPItests/acctok.t
 
 # Variables
 course_id = '2110' 
-file_path_list = ["outcanvas/OCW_function_reflections.zip"]
+file_path_list = ["outcanvas/ET_09_04.zip"]
 
 ######## FOR UPLOADING ETS
 # fpl = []
--- algtwo2026/daily_materials/day005/exams2canvas_mixed.r
+++ algtwo2026/daily_materials/day005/exams2canvas_mixed.r
@@ -3,17 +3,18 @@ n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-# pm = list("et01_are_points_a_function.Rmd",
-#           "et02_two-step_inverse_alg.Rmd")
-# 
-# exams2canvas(pm,
-#              n=n,
-#              dir=outdir,
-#              points=c(2,2),
-#              maxattempts = 6,
-#              name="ET_09_03",
-#              template = "canvas_qti12.xml",
-#              cloze = list(enumerate = FALSE))
+pm = list("et01_are_points_a_function.Rmd",
+          "et02_two-step_inverse_alg.Rmd",
+          "et03_graph_inverse.Rmd")
+
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=c(1,1,1),
+             maxattempts = 6,
+             name="ET_09_04",
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
 
 
 # Classwork 1
@@ -34,16 +35,16 @@ outdir = "outcanvas"
 #              cloze = list(enumerate = FALSE))
 
 # Classwork 2
-pm = list("01_f_negs_table_easier.Rmd",
-          "02_fun_reflects.Rmd")
-
-exams2canvas(pm,
-             n=n,
-             dir=outdir,
-             points=c(4,4),
-             maxattempts = 8,
-             name="OCW_function_reflections",
-             template = "canvas_qti12.xml",
-             cloze = list(enumerate = FALSE))
-
+# pm = list("01_f_negs_table_easier.Rmd",
+#           "02_fun_reflects.Rmd")
+# 
+# exams2canvas(pm,
+#              n=n,
+#              dir=outdir,
+#              points=c(4,4),
+#              maxattempts = 8,
+#              name="OCW_function_reflections",
+#              template = "canvas_qti12.xml",
+#              cloze = list(enumerate = FALSE))
+# 
 
