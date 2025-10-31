0eefb3067d03f1f1ed3d58d46f8ec9f0c6329c95
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-10-31 13:45:56 -0400

    hmm hi

---
+++ blooket/quad_factor_a=prime_TYPING/blooket_autogui_typing.py
@@ -0,0 +1,56 @@
+import pyautogui as pag
+import time
+# time.sleep(5)
+print(pag.position())
+# print(pag.size())
+pag.PAUSE = 0.8 #wait after each click
+# pyautogui.FAILSAFE = True #KEEP THIS TRUE
+
+file = open("p3_solutions.txt", "r")
+lines = file.readlines()
+sols = []
+for line in lines:
+    sols.append(line.strip())
+file.close()
+
+switch_win_pos = pag.Point(339,525)
+add_q_pos = pag.Point(108,760)
+qt_pos = pag.Point(430,381)
+MC_to_type_pos = pag.Point(464,225)
+TA_pos = pag.Point(410,532)
+Image_pos = pag.Point(136,307)
+Upload_a_File_pos = pag.Point(381,446)
+Open_pos = pag.Point(1023,619)
+Answer_pos = pag.Point(378,601)
+Save_pos = pag.Point(711,228)
+AAA = pag.Point(440,671)
+
+pag.click(switch_win_pos)
+for i in range(400):
+    ifn = "q{:03d}.png".format(i+1)
+    ans = sols[i]
+    ans1 = ans.split("#")[0]
+    ans2 = ans.split("#")[1]
+    if(ans != "NaN"):
+        pag.click(add_q_pos)
+        pag.click(qt_pos)
+        pag.typewrite('Factor.',interval=0)
+        pag.click(MC_to_type_pos)
+        pag.click(TA_pos)
+        pag.click(Image_pos)
+        pag.click(Upload_a_File_pos)
+        time.sleep(2)
+        pag.typewrite(ifn,interval=0.1)
+        pag.click(Open_pos)
+        pag.click(Answer_pos)
+        pag.typewrite(ans1,interval=0)
+        if ans1 != ans2:
+            pag.click(AAA)
+            time.sleep(0.1)
+            pag.click(AAA)
+            pag.typewrite(ans2,interval=0)
+            time.sleep(0.1)
+        pag.click(Save_pos)
+        time.sleep(6)
+
+
---
+++ blooket/quad_factor_a=prime_TYPING/quad_factor_a=1_typing_ALL.R
@@ -0,0 +1,71 @@
+library(tinytex)
+
+pn = function(c,d){
+    cc = c[c!=0]
+    dd = d[c!=0]
+    p = paste0("{",cc,"}x^{",dd,"}",collapse="+")
+    p = gsub("x^{0}","",p,fixed=T)
+    p = gsub("x^{1}","x",p,fixed=T)
+    p = gsub("+{-","-{",p,fixed=T)
+    p = gsub("+{1}x","+x",p,fixed=T)
+    p = gsub("-{1}x","-x",p,fixed=T)
+    p = gsub("{-1}x","-x",p,fixed=T)
+    p = gsub("{1}x","x",p,fixed=T)
+    return(p)
+}
+
+getsols = function(abcd){
+    A = abcd[1]
+    B = abcd[2]
+    C = abcd[3]
+    D = abcd[4]
+    sol1 = paste0("(",A,"x+",B,")(",C,"x+",D,")")
+    sol1 = gsub("+-","-",sol1,fixed=T)
+    sol1 = gsub("1x","x",sol1,fixed=T)
+    sol2 = paste0("(",C,"x+",D,")(",A,"x+",B,")")
+    sol2 = gsub("+-","-",sol2,fixed=T)
+    sol2 = gsub("1x","x",sol2,fixed=T)
+    sol3 = paste0("(",-A,"x+",-B,")(",-C,"x+",-D,")")
+    sol3 = gsub("+-","-",sol3,fixed=T)
+    sol3 = gsub("1x","x",sol3,fixed=T)
+    sol4 = paste0("(",-C,"x+",-D,")(",-A,"x+",-B,")")
+    sol4 = gsub("+-","-",sol4,fixed=T)
+    sol4 = gsub("1x","x",sol4,fixed=T)
+    # sols = paste0(c(sol1,sol2,sol3,sol4),collapse="#")
+    sols = paste0(c(sol1,sol2),collapse="#")
+}
+
+gcd2 <- function(a, b) {
+    if (b == 0) a else Recall(b, a %% b)
+}
+gcd <- function(...) Reduce(gcd2, c(...))
+
+s=""
+
+i=0
+for(B in c(-10:-1,1:10)){
+    for(D in c(-10:-1,1:10)){
+        i = i+1
+        A = 1
+        C = 1
+        abcd = c(A,B,C,D)
+        a = A*C
+        b = B*C+A*D
+        c = B*D
+        # myex = paste0(pn(c(a,b,c),2:0))
+        # s2 = "\\documentclass[border=5pt]{standalone}\n\\begin{document}\n$myex$\n\\end{document}"
+        # s2 = gsub("myex",myex,s2,fixed=T)
+        # fileConn<-file("tmp.tex")
+        # writeLines(s2, fileConn)
+        # close(fileConn)
+        # xelatex("tmp.tex")
+        # cmand = "magick -density 600 tmp.pdf -background white -flatten FOPNG"
+        # ofn = paste0("p3/q",sprintf("%03d",i),".png")
+        # cmand = gsub("FOPNG",ofn,cmand,fixed=T)
+        # system(cmand)
+        s = paste0(s,getsols(abcd),"\n",collapse="")
+    }
+}
+
+fileConn<-file("p3_solutions.txt")
+writeLines(s, fileConn)
