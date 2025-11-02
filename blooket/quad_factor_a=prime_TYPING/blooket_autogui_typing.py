c3a0ae1adb9ac9e2e4873095b7aad5d55f463780
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-11-02 13:15:48 -0500

    hmm hi

--- blooket/quad_factor_a=prime_TYPING/blooket_autogui_typing.py
+++ blooket/quad_factor_a=prime_TYPING/blooket_autogui_typing.py
@@ -6,7 +6,7 @@ print(pag.position())
 pag.PAUSE = 0.8 #wait after each click
 # pyautogui.FAILSAFE = True #KEEP THIS TRUE
 
-file = open("p3_solutions.txt", "r")
+file = open("p1_solutions.txt", "r")
 lines = file.readlines()
 sols = []
 for line in lines:
@@ -26,28 +26,38 @@ Save_pos = pag.Point(711,228)
 AAA = pag.Point(440,671)
 
 pag.click(switch_win_pos)
-for i in range(400):
+for i in range(1000):
     ifn = "q{:03d}.png".format(i+1)
     ans = sols[i]
     ans1 = ans.split("#")[0]
     ans2 = ans.split("#")[1]
     if(ans != "NaN"):
         pag.click(add_q_pos)
+        time.sleep(0.1)
         pag.click(qt_pos)
+        time.sleep(0.1)
         pag.typewrite('Factor.',interval=0)
+        time.sleep(0.1)
         pag.click(MC_to_type_pos)
+        time.sleep(0.1)
         pag.click(TA_pos)
+        time.sleep(0.1)
         pag.click(Image_pos)
+        time.sleep(0.1)
         pag.click(Upload_a_File_pos)
         time.sleep(2)
         pag.typewrite(ifn,interval=0.1)
         pag.click(Open_pos)
+        time.sleep(0.1)
         pag.click(Answer_pos)
+        time.sleep(0.1)
         pag.typewrite(ans1,interval=0)
+        time.sleep(0.1)
         if ans1 != ans2:
             pag.click(AAA)
             time.sleep(0.1)
             pag.click(AAA)
+            time.sleep(0.1)
             pag.typewrite(ans2,interval=0)
             time.sleep(0.1)
         pag.click(Save_pos)
