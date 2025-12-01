b37b99acf5fc327057036757fc96138fd2a925cd
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-BART-Charter-Public-School-2023.local>
2025-11-30 21:00:40 -0500

    hmm hi

--- algtwo2026/u05/1201/exams2forms_testing.R
+++ algtwo2026/u05/1201/exams2forms_testing.R
@@ -2,15 +2,18 @@ library("exams2forms")
 n = 8
 
 ### Entrance ticket
-pm = list("complete_the_square_F.Rmd",
-          "flip_parabola.Rmd",
-          "vertex_form_animation.Rmd")
+pm = list("et01_EZ_two-step_equations.Rmd",
+          "et02_shift_squaring.Rmd",
+          "et03_intervals_inc_dec_pos_neg_quad.Rmd",
+          "et04_solve_a1_standard_eq_0.Rmd",
+          "quad_formula_decimal.Rmd",
+          "cannon_balloon.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "at1124canvas_practice",
+             title = "examples_1201",
              dir = ".",
-             name="at1124canvas_practice")
+             name="examples_1201")
 
 
 
